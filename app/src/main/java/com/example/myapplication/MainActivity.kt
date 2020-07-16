47b9950f5600f6e6cfaa9a2baebd858df734a2e8
Alabh-Sumaniya <alabh.sumaniya9@gmail.com>
2020-07-17 00:15:28 +0530

    Kotlin Intent Program

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,6 +19,17 @@ class MainActivity : AppCompatActivity() {
         Log.i("MainActivity","Button was clicked")
 
             Toast.makeText(applicationContext,"Button was clicked.!",Toast.LENGTH_LONG).show()
+        }
+
+
+        buttonMessage.setOnClickListener {
+
+            val message : String= editText.text.toString()
+
+            Toast.makeText(applicationContext,message,Toast.LENGTH_SHORT).show()
+
+            val intent=Intent(this,SecondActivity::class.java)
+            startActivity(intent)
         }
 
     }
